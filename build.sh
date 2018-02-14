28f148a7b8cc0ed995ae4ff30669bfd910712934
Jioh L. Jung <ziozzang@gmail.com>
2018-02-15 08:50:55 +0900

    Add Target BIN_NAME to echo

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ BIN_NAME="${PWD##*/}"
 BUILD_DIRS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GO_VERSION=${GO_VERSION:-latest}
 
-echo ">> Working Directory detected: $BUILD_DIRS"
+echo ">> Working Directory detected: '$BUILD_DIRS'"
+echo ">> Target Binaryname: '$BIN_NAME'"
 echo ">> Running Docker images..."
 docker run -it --rm \
   -v ${BUILD_DIRS}/:/usr/src/${BIN_NAME} \
