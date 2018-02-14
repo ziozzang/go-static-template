2b66a24061df3bdf5d450e834f2b631431d0ae03
Jioh L. Jung <ziozzang@gmail.com>
2018-02-15 08:34:43 +0900

    fix directory issue

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 # Code  by Jioh L. Jung<ziozzang@gmail.com>
 
 BIN_NAME="${PWD##*/}"
-BUILD_DIRS=`pwd`
+BUILD_DIRS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GO_VERSION=${GO_VERSION:-latest}
 
+echo ">> Working Directory detected: $BUILD_DIRS"
+echo ">> Running Docker images..."
 docker run -it --rm \
-  -v ${BUILD_DIRS}:/usr/src/${BIN_NAME} \
+  -v ${BUILD_DIRS}/:/usr/src/${BIN_NAME} \
+  -v ${BUILD_DIRS}/.pkgs/:/go/pkg \
   -w /usr/src/${BIN_NAME} \
   golang:${GO_VERSION} bash make
 
